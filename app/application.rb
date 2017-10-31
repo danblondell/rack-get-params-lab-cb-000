8dfd4f214e9caf91806b132eae38ed806d0ed2ba
Dan Blondell <danblondell@gmail.com>
2017-10-31 03:39:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
 
     elsif req.path.match(/add/)
       added_item = resp.get(item)
-      if @@items.include(added_item)
+      if @@items.include?(added_item)
         @@cart << added_item
         resp.write "added #{added_item}"
       else
