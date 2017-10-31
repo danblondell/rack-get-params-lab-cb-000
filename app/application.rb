a97b1d54b53ece4279f10794b999d85a00e0532d
Dan Blondell <danblondell@gmail.com>
2017-10-31 03:33:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,13 @@ class Application
       resp.write "Path Not Found"
     end
 
+    if req.path.match(/cart/)
+      @@cart.each do |cart_item|
+        resp.write "#{cart_item}\n"
+      end
+    end
+      
+
     resp.finish
   end
 
